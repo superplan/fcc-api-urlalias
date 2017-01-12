5cfb60b3c191fe1aa1463d43cb4ab2bd1017ddc8
Michael K <michael.kamfor@tu-dortmund.de>
2017-01-12 13:47:28 +0000

    homepage + basic logic

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.set('view engine', 'pug');
 
 //Global variables
 var myPort = process.env.PORT;
-// var myHost = process.env.IP;
 
 // location of HTML templates
 app.set('views', __dirname + '/views');
@@ -27,6 +26,47 @@ app.get('/', function(req, res) {
 
 });
 
+//Middleware: service
+app.get('/:input', function (req, res) {
+  
+  //Initialize variables
+  var outputJSON = {"your_alias": null, "original_url": req.params.input};
+  var outputERR = {"error": null};
+  var argIsInt = isInt(req.params.input);
+  var argIsUrl = isStr(req.params.input);
+  
+  //Input date to unix format
+  if (argIsInt){
+    // Handle integer input
+    outputERR.error = "int";
+  } else if (argIsUrl){
+    //Handle non integer input
+    outputERR.error = "url";
+  } else {
+    //Handle errors
+    outputERR.error = "err";
+  }
+  
+  //Write response
+  console.log(outputERR);
+  res.send(outputERR);
+});
+
+// start App
 app.listen(myPort, function () {
   console.log('Example app listening on port '+myPort+'!');
 });
+
+
+/*
+ * Functions
+ */
+function isInt(input){
+  var regNumbersOnly = new RegExp('^[0-9]+$');
+  if (regNumbersOnly.test(input)) return parseInt(input,10);
+  else return false;
+}
+
+function isStr(input){
+  return true;
+}
